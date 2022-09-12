2118044b0c831b409278748c87c920e53b818d83
hermantretteteig <herman.tretteteig@hotmail.com>
2022-09-12 18:15:20 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { DataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import classes from './App.module.css'
+import { Button } from '@dhis2/ui'
+
+import Mainpage from './Mainpage';
+
+const query = {
+    me: {
+        resource: 'me',
+    },
+}
+
+
+const redirect = () => {
+    const message = "Hello!\nHere is you report for Kasungo district.\n\nhttps://vg.no".replace(" ","%20")
+    window.location.replace("https://wa.me/+4746802013?text="+message);
+}
+
+const MyApp = () => {
+    
+    
+    useEffect(() => {
+        loadChart();
+      }, [])
+      
+      
+      const loadChart = () => {
+       /* ChartPlugin.url = "https://play.dhis2.org/demo";
+          chartPlugin.username = "admin";
+          chartPlugin.password = "district";
+          chartPlugin.loadingIndicator = true;
+      
+          var r1 = { el: "report1", id: "R0DVGvXDUNP" };
+      
+                  // Chart configuration, render to "report2" div
+      
+                  var r2 = {
+                      el: "report2",
+                      columns: [
+                          {
+                              dimension: "dx",
+                              items: [{ id: "YtbsuPPo010" }, { id: "l6byfWFUGaP" }],
+                          },
+                      ],
+                      rows: [{ dimension: "pe", items: [{ id: "LAST_12_MONTHS" }] }],
+                      filters: [{ dimension: "ou", items: [{ id: "USER_ORGUNIT" }] }],
+      
+                      // All following properties are optional
+                      title: "Custom title",
+                      type: "line",
+                      showValues: false,
+                      hideEmptyRows: true,
+                      regressionType: "LINEAR",
+                      completedOnly: true,
+                      targetLineValue: 100,
+                      targetLineTitle: "My target line title",
+                      baseLineValue: 20,
+                      baseLineTitle: "My base line title",
+                      aggregationType: "AVERAGE",
+                      rangeAxisMaxValue: 100,
+                      rangeAxisMinValue: 20,
+                      rangeAxisSteps: 5,
+                      rangeAxisDecimals: 2,
+                      rangeAxisTitle: "My range axis title",
+                      domainAxisTitle: "My domain axis title",
+                      hideLegend: true,
+                  };
+      
+                  // Render the charts
+      
+                  chartPlugin.load(r1, r2);
+      */
+      }
+      
+    
+    return(
+
+    
+    <div className={classes.container}>
+        <DataQuery query={query}>
+            
+            {({ error, loading, data }) => {
+                if (error) return <span>ERROR</span>
+                if (loading) return <span>...</span>
+                return (
+                    <>
+                      <Mainpage/>
+                      {/* <Button onClick={redirect}>Send on whatsapp!</Button>*/}
+                    </>
+                )
+            }}
+        </DataQuery>
+    </div>
+)
+        }
+
+export default MyApp
---
+++ src/Mainpage.jsx
@@ -0,0 +1,168 @@
+import * as analytics from '@dhis2/analytics'
+import React, {useState, useRef, useEffect} from 'react'
+import PropTypes from 'prop-types'
+import ChartPlugin from "@dhis2/data-visualizer-plugin"
+//import { useSelector } from 'react-redux'
+//import { sGetChart } from './chart.js'
+import { useDataMutation } from '@dhis2/app-runtime'
+const Mainpage = (e) => {
+
+    const [imageURL, setImageURL] = useState("https:example.com") 
+    const [convertSVG, setConvertSVG] = useState(false);
+    const [message, setMessage] = useState("Not loading...");
+
+    //const chart = useSelector(sGetChart)
+
+
+    const dxMock = {
+        dimension: 'dx',
+        items: [
+            {
+                id: 'cYeuwXTCPkU',
+            },
+        ],
+    }
+
+    const peMock = {
+        dimension: 'pe',
+        items: [
+            {
+                id: 'LAST_12_MONTHS',
+            }
+        ],
+    }
+    
+    const ouMock = {
+        dimension: 'ou',
+        items: [
+            {
+                id: 'at6UHUQatSo',
+            }
+        ],
+    }
+
+    const firstUpdate = useRef(true);
+    useEffect(() => {
+        if (firstUpdate.current) {
+            firstUpdate.current = false;
+            return;
+        }
+
+        onSVGButton();
+    }, [convertSVG])
+
+    const mockExtraOptions = {
+        dashboard: false,
+        noData: {
+            text: 'No data',
+        },
+    }
+
+
+    
+
+    const getImageURL = async () => {
+        //return "hri"
+        console.log("print")
+        //setMessage("Svg is loading..")
+        
+        //const res = createVisualizationMock.visualization.getSVGForExport();
+        //console.log(res);
+        const element = document.getElementById("234").children[0].children[0].children[0].children[0].innerHTML//.children[0];
+        //console.log(element)
+
+        let formData = new URLSearchParams();
+        formData.append('filename', 'png-image');
+        formData.append('svg', element);
+
+        let imgUrl = "";
+
+        
+        const host = "https://play.dhis2.org/dev"
+        //const host = "http://localhost:9999"
+        const res = await fetch(host+"/api/38/svg.png",
+        {
+            method: "post",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}, 
+            body: formData
+        }).then(response => response.blob()).catch(error => {
+            //setMessage(error.message)
+        })
+        .then(imageBlob => {
+            // Then create a local URL for that image and print it 
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            imgUrl = imageObjectURL;
+            console.log("URL")
+            console.log(imgUrl)
+            //return "ooko"
+            //return imgUrl
+            //setImageURL(imgUrl);
+            //setMessage("finish");
+        });
+        console.log(res)
+        
+
+    }
+
+    const createVisualizationMock = {
+
+
+
+        visualization: {
+            getSVGForExport: () => '<svg />',
+        },
+        config: {
+            getConfig: () => {},
+        },
+    }
+
+ 
+    
+
+    const props = {
+        style: { height: 400, width : 300 },
+        id: 1,
+        onChartGenerated: async () => {
+            console.log(await getImageURL())
+            //setImageURL((() => getImageURL(
+        },//onSVGButton(),
+        responses: [],
+        extraOptions: mockExtraOptions,
+        legendSets: [],
+        forDashboard : true,
+        visualization : {
+            type: analytics.VIS_TYPE_BAR,
+            columns: [dxMock],
+            rows: [peMock],
+            filters: [ouMock],
+        }//something
+    }
+
+  return (
+    <>
+    <div id="234">
+
+        <ChartPlugin {...props}/>
+       
+       {/* <ChartPlugin {...advancedProps}/>*/}
+     
+    </div>
+
+    
+      
+        <p>{imageURL}<br/></p>
+      <ol type="1">
+        <li><a href={imageURL} download>Save image to your phone</a></li>
+        <li><a href="https://wa.me/+4746802013?text=Here is your report:">Open whatsapp!</a></li>
+        <li>Send a message, togheter with the image</li>
+        </ol> 
+
+    
+      {/*<img src={imageURL}/>*/}
+      </>
+  )
+}
+
+Mainpage.propTypes = {}
+
+export default Mainpage
